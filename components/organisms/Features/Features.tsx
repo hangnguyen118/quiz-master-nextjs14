1af92ed0f011c7aa43b93c7514e60756a0757d5f
hangnguyen118 <hangnguyen118.wy@gmail.com>
2024-10-21 02:12:58 +0700

    edit file features component

--- components/organisms/Features/Features.tsx
+++ components/organisms/Features/Features.tsx
@@ -34,21 +34,21 @@ const mockdata = [
 ];
 
 export default function Features() {
-  const features = mockdata.map((feature) => (
-    <Card key={feature.title} shadow="md" radius="md" className={classes.card} padding="xl">
-      <feature.icon
-        style={{ width: rem(50), height: rem(50) }}
-        stroke={2}
-        color="lime"
-      />
-      <Text fz="lg" fw={500} className={classes.cardTitle} mt="md">
-        {feature.title}
-      </Text>
-      <Text fz="sm" c="dimmed" mt="sm">
-        {feature.description}
-      </Text>
-    </Card>
-  ));
+  // const features = mockdata.map((feature) => (
+  //   <Card key={feature.title} shadow="md" radius="md" className={classes.card} padding="xl">
+  //     <feature.icon
+  //       style={{ width: rem(50), height: rem(50) }}
+  //       stroke={2}
+  //       color="lime"
+  //     />
+  //     <Text fz="lg" fw={500} className={classes.cardTitle} mt="md">
+  //       {feature.title}
+  //     </Text>
+  //     <Text fz="sm" c="dimmed" mt="sm">
+  //       {feature.description}
+  //     </Text>
+  //   </Card>
+  // ));
 
   return (
     <Container size="lg" py="xl">
